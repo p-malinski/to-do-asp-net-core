1641da4edd929cd1850af14631cbecef158d89bd
Przemysław Maliński <130255018+p-malinski@users.noreply.github.com>
2024-11-09 23:09:58 +0100

    Revert "Feature/001 configure db context"

--- ToDoAspNetCore/Program.cs
+++ ToDoAspNetCore/Program.cs
@@ -1,13 +1,7 @@
-using Microsoft.EntityFrameworkCore;
-using ToDoAspNetCore.Persistence;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
-builder.Services.AddDbContext<AppDbContext>(
-       options => options.UseSqlServer(builder.Configuration.GetConnectionString("DbSqlServer")));
 
 var app = builder.Build();
 
