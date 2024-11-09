4ef45ea65c965882c6ba0f65775c4a2fd2efe766
Przemysław Maliński <130255018+p-malinski@users.noreply.github.com>
2024-11-09 23:09:27 +0100

    Add ToDoItems table migration

--- ToDoAspNetCore/Models/ToDoItemModel.cs
+++ ToDoAspNetCore/Models/ToDoItemModel.cs
@@ -5,6 +5,6 @@
         public int Id { get; set; }
         public string Text { get; set; }
         public bool IsDone { get; set; }
-        DateTime DueDate { get; set; }
+        public DateTime DueDate { get; set; }
     }
 }
